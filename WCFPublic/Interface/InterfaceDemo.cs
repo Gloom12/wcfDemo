487015bfcb314bb7ff81fea6ab8efdb6e32ae5ea
Gloom12 <38236408+Gloom12@users.noreply.github.com>
2022-02-16 18:09:30 +0800

    增加服务以及测试应用程序

---
+++ WCFPublic/Interface/InterfaceDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WCFPublic.Interface
+{
+    /// <summary>
+    /// wcf接口
+    /// </summary>
+    [ServiceContract(Name = "WCFDemo")]
+    public interface InterfaceDemo
+    {
+        [OperationContract]
+        string SendMassege(string massege);
+        [OperationContract]
+        string ReadMassege(string massege);
+        
+    }
+}
